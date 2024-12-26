cd73543885b00a6258b7316df6bf0926f63a8661
Kavya16890 <131844765+Kavya16890@users.noreply.github.com>
2024-12-26 14:30:54 +0000

    initial commit

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './index.css';
+import TodoList from './Components /Todolist';
+
+function App() {
+  return (
+    <>
+    <TodoList/>
+    </>
+  )
+}
+
+export default App;
